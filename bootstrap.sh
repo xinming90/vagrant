b5c7cf9555206409b6f3ae767a331163fbeaa95d
xinming90 <panxinming90@gmail.com>
2015-04-05 19:04:30 +0800

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,18 @@
+# system
 sudo sed -i s/archive.ubuntu.com/mirrors.yun-idc.com/g /etc/apt/sources.list
 sudo sed -i s/security.ubuntu.com/mirrors.yun-idc.com/g /etc/apt/sources.list
 sudo apt-get update
 
-sudo apt-get install -y git python-dev python-pip
+# 
+sudo apt-get install -y default-jdk htop git gcc
+
+# python
+sudo apt-get install -y python-dev python-pip
 sudo pip install virtualenv
 virtualenv ~/virtualenv
 echo ". ~/virtualenv/bin/activate" >> ~/.bashrc
 . ~/virtualenv/bin/activate
 pip install ipython
+pip install httpie
 
 #curl https://raw.githubusercontent.com/xinming90/vim/master/install.sh | sh -x
