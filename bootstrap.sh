aff2224b5cffe47864c8ee260aeb692e3d2e1ad7
xinming90 <panxinming90@gmail.com>
2015-06-21 21:22:20 +0800

    pip install sqlalchemy

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,6 @@ sudo apt-get install -y default-jdk htop git gcc htop
 sudo apt-get install -y emacs zile
 
 
-# python
-sudo apt-get install -y python-dev python-pip
-sudo pip install virtualenv -i http://pypi.douban.com/simple
-virtualenv ~/virtualenv
-echo ". ~/virtualenv/bin/activate" >> ~/.bashrc
-echo 'alias tmysql="sudo tail -f /var/log/mysql/mysql.log"' >> ~/.bashrc
-. ~/virtualenv/bin/activate
-pip install ipython -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-pip install httpie -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-pip install pymysql -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-
-
 # mysql
 sudo apt-get install -y mysql-server
 sudo sed -i 's/^#general_log_file/general_log_file/' /etc/mysql/my.cnf
@@ -36,3 +24,16 @@ echo 'ZSH_THEME="robbyrussell"' >> ~/.zshrc
 echo "plugins=(git)" >> ~/.zshrc
 echo 'export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin"' >> ~/.zshrc
 echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
+
+
+# python
+sudo apt-get install -y python-dev python-pip
+sudo pip install virtualenv -i http://pypi.douban.com/simple
+virtualenv ~/virtualenv
+echo ". ~/virtualenv/bin/activate" >> ~/.zshrc
+echo 'alias tmysql="sudo tail -f /var/log/mysql/mysql.log"' >> ~/.zshrc
+. ~/virtualenv/bin/activate
+pip install ipython -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install httpie -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install pymysql -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install sqlalchemy -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
